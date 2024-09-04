e12a1a91dd84ac77f316961284acc6c4257ac099
FrancoSimonFormia <franco.formia@alumnos.frgp.utn.edu.ar>
2024-09-04 07:10:37 -0300

    Clase Persona

---
+++ TP3_GRUPO_10/src/ejercicio1/Persona.java
@@ -0,0 +1,17 @@
+package ejercicio1;
+
+public class Persona {
+
+	private String apellido;
+	private String nombre;
+	private int dni;
+	
+	public Persona() {
+	}
+	
+	public Persona(String apellido, String nombre, int dni) {
+		this.apellido = apellido;
+		this.nombre = nombre;
+		this.dni = dni;
+	}
+}
